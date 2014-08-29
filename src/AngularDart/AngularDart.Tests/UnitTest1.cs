a6909716d5dd21efdd0318788d162b7460ce94a5
Darren Reid <layoric@gmail.com>
2014-08-29 21:08:05 +1000

    Initial project commit

---
+++ src/AngularDart/AngularDart.Tests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using NUnit.Framework;
+using AngularDart.ServiceInterface;
+using AngularDart.ServiceModel;
+using ServiceStack.Testing;
+using ServiceStack;
+
+namespace AngularDart.Tests
+{
+    [TestFixture]
+    public class UnitTests
+    {
+        private readonly ServiceStackHost appHost;
+
+        public UnitTests()
+        {
+            appHost = new BasicAppHost(typeof(MyServices).Assembly)
+            {
+                ConfigureContainer = container =>
+                {
+                    //Add your IoC dependencies here
+                }
+            }
+            .Init();
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            appHost.Dispose();
+        }
+
+        [Test]
+        public void TestMethod1()
+        {
+            var service = appHost.Container.Resolve<MyServices>();
+
+            var response = (HelloResponse)service.Any(new Hello { Name = "World" });
+
+            Assert.That(response.Result, Is.EqualTo("Hello, World!"));
+        }
+    }
+}
